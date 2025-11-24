75e832b918753e6c5a0ad5a8c5129f4dcb3bfe41
lagishtein <lagishtein@gmail.com>
2025-11-24 13:17:47 -0500

    Submit homework

--- src/main/java/exercises/BasicBag.java
+++ src/main/java/exercises/BasicBag.java
@@ -6,23 +6,42 @@ import edu.touro.mcon264.apps.collections.BagInterface;
 public class BasicBag<T> extends ArrayCollection<T> implements BagInterface<T> {
     @Override
     public T grab() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        T element =  elements[(int) (Math.random() * numElements)];
+        remove(element);
+        return element;
     }
 
     @Override
     public int count(T target) {
-        return 0;
+       int count = 0;
+         for (int i = 0; i < numElements; i++) {
+            if (elements[i].equals(target)) {
+                count++;
+            }
+         }
+            return count;
     }
 
     @Override
     public int removeAll(T target) {
-        return 0;
+        int removed = 0;
+        while (remove(target)) {
+            removed++;
+        }
+        return removed;
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < numElements; i++) {
+            elements[i] = null;
+        }
+        numElements = 0;
     }
+
 
     // TODO: implement the methods in the BagInterface using an array as the underlying data structure
     // consider extending ArrayCollection<T>
--- src/main/java/exercises/ExtendedLinkedCollection.java
+++ src/main/java/exercises/ExtendedLinkedCollection.java
@@ -1,19 +1,54 @@
 package exercises;
 
 import edu.touro.mcon264.apps.collections.LinkedCollection;
-
+import edu.touro.mcon264.support.LLNode;
 public class ExtendedLinkedCollection<T> extends LinkedCollection<T> {
     @Override
     public String toString() {
-      //ТОDO: implement this method. Use StringBuider. Create Javadoc.
+        StringBuilder sb = new StringBuilder();
+        sb.append("ExtendedLinkedCollection: [");
+        LLNode current = head;
+        while (current != null) {
+            sb.append(current.getInfo());
+            if (current.getLink() != null) {
+                sb.append(", ");
+            }
+            current = current.getLink();
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
 
     public int count(T target) {
-      //ТОDO: implement this method. Count the number of times target appears in the collection. Create Javadoc.
+        LLNode<T> current = this.head;
+        int count = 0;
+        while (current != null) {
+            if (current.getInfo().equals(target)) {
+                count++;
+            }
+            current = current.getLink();
+        }
+        return count;
     }
 
     public void removeAll(T target) {
-      //ТОDO: implement this method. Remove all occurrences of target from the collection and return the number of elements removed. Create Javadoc.
+        LLNode<T> current = this.head;
+        LLNode<T> previous = null;
+
+        while (current != null) {
+            if (current.getInfo().equals(target)) {
+                if (previous == null) {
+                    // Removing head
+                    head = current.getLink();
+                } else {
+                    previous.setLink(current.getLink());
+                }
+                numElements--;
+            } else {
+                previous = current;
+            }
+            current = current.getLink();
+        }
     }
 }
